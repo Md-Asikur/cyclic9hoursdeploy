ac50960863d67e66a8e8f9e30edf442e1987584c
Md-Asikur <asikurrahaman997@gamil.com>
2022-12-13 02:22:09 -0800

    hello

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ const bodyParser = require('body-parser')
 //   );
 //   next();
 // });
-app.use(express.json())
-
-app.use(bodyParser.json());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb" }));
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({ origin: "https://bandanna-scarab.cyclic.app/" }));
 app.use(fileUpload({
     useTempFiles: true
 }))
